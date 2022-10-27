a6938339889837d519e027ff24e9f6cbc47d7a47
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2022-10-26 19:29:04 -0700

    rm instructions

--- ouroboros.py
+++ ouroboros.py
@@ -97,7 +97,6 @@ HTML_SKELETON = """
   </div>
 </section>
 <section class="instructions">
-  <p>Click and drag</p>
   </section>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide-extension-auto-scroll@0.5.3/dist/js/splide-extension-auto-scroll.min.js"></script>
